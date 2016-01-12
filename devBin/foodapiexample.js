fcfcfb2bd6f153a4dc6b432862de38768e2a4388
Mauricio Andrades <mod.and.op@gmail.com>
2016-01-11 16:14:37 -0800

    created devBin, added codepen files

---
+++ devBin/foodapiexample.js
@@ -0,0 +1,40 @@
+/**
+ * this is an example json object from the food-api website.
+ * It's just for testing and playing around.
+ */
+var jsonObj = [{
+  "id": 641803,
+  "title": "Easy & Delish! ~ Apple Crumble",
+  "image": "https://spoonacular.com/recipeImages/Easy---Delish--Apple-Crumble-641803.jpg",
+  "usedIngredientCount": 3,
+  "missedIngredientCount": 4,
+  "likes": 1
+}, {
+  "id": 645152,
+  "title": "Grandma's Apple Crisp",
+  "image": "https://spoonacular.com/recipeImages/Grandmas-Apple-Crisp-645152.jpg",
+  "usedIngredientCount": 3,
+  "missedIngredientCount": 6,
+  "likes": 1
+}, {
+  "id": 657563,
+  "title": "Quick Apple Ginger Pie",
+  "image": "https://spoonacular.com/recipeImages/Quick-Apple-Ginger-Pie-657563.jpg",
+  "usedIngredientCount": 3,
+  "missedIngredientCount": 6,
+  "likes": 1
+}, {
+  "id": 639487,
+  "title": "Cinnamon Sugar Fried Apples",
+  "image": "https://spoonacular.com/recipeImages/Cinnamon-Sugar-Fried-Apples-639487.jpg",
+  "usedIngredientCount": 3,
+  "missedIngredientCount": 8,
+  "likes": 46
+}, {
+  "id": 643426,
+  "title": "Fresh Apple Cake With Caramel Sauce",
+  "image": "https://spoonacular.com/recipeImages/Fresh-Apple-Cake-With-Caramel-Sauce-643426.jpg",
+  "usedIngredientCount": 3,
+  "missedIngredientCount": 12,
+  "likes": 9
+}];
